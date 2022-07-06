03de554ac3e6086a83f9e1366b7c1a8954e19956
Stef Tervelde <stef@steftervelde.nl>
2022-07-06 16:07:58 +0200

    swiftpm

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/Stefterv/filament", branch: "main")
+        .package(url: "https://github.com/Stefterv/filament", branch: "swift_pm")
     ],
     targets: [
         .target(
