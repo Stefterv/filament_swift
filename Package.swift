def5e571ed7d17fa0c70f393f703c46c5099340c
Stef Tervelde <stef@steftervelde.nl>
2023-09-09 11:59:32 +0200

    Added filament without modifying the original repo

--- Package.swift
+++ Package.swift
@@ -2,20 +2,20 @@
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
+let targets = [
+    "libbackend","libbasis_transcoder","libcamutils","libcivetweb","libdracodec","libfilabridge","libfilaflat","libfilamat","libfilamat_lite","libfilament-iblprefilter","libfilament","libfilameshio","libgeometry","libgltfio_core","libibl-lite","libibl","libimage","libktxreader","libmeshoptimizer","libmikktspace","libshaders","libsmol-v","libstb","libuberarchive","libuberzlib","libutils","libviewer","libvkshaders","libzstd"
+]
 
 let package = Package(
     name: "filament_swift",
     products: [
         .library(
-            name: "Filament",
-            targets: ["Filament"]),
+            name: "filament",
+            targets: ["FilamentBindings", "Helpers"] + targets),
     ],
-    dependencies: [
-        .package(url: "https://github.com/Stefterv/filament", exact: "1.25.0")
-    ],
-    targets: [
+    targets: ([
         .target(
-            name: "Filament",
+            name: "Helpers",
             dependencies: [
                 "FilamentBindings"
             ],
@@ -23,14 +23,8 @@ let package = Package(
         ),
         .target(
             name: "FilamentBindings",
-            dependencies: [
-                "filament",
-            ],
-            path: "Bindings",
-            cxxSettings: [
-                .headerSearchPath("../include")
-            ]
-        ),
-    ],
+            path: "Bindings"
+        )
+    ] + targets.map({ .binaryTarget(name: $0, path: "lib/\($0).xcframework") })),
     cxxLanguageStandard: .cxx17
 )
---
+++ release.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+set -e
+
+VERSION="1.42.1"
+
+if [ ! -d "filament" ]
+then
+    git clone https://github.com/google/filament.git filament
+else
+    git -C filament fetch
+fi
+git -C filament checkout tags/v${VERSION}
+
+filament/build.sh -p ios -s release
+
+LEADER_PATH="filament/out/ios-release/filament/lib/arm64"
+OUTPUT_DIR="lib/"
+
+# Loop through each file in the leader path. For each library we find, we'll collect additional
+# architectures in the other paths and combine them all into a universal library.
+for FILE in "${LEADER_PATH}"/*.a; do
+    [ -f "${FILE}" ] || continue
+
+    LIBRARY_NAME=$(basename "${FILE%.*}")
+
+    echo "Creating xcframework for library: ${LIBRARY_NAME}"
+    INPUT_FILES=("-library ${LEADER_PATH}/${LIBRARY_NAME}.a")
+
+    if [[ $LIBRARY_NAME == "libfilament" ]]; then
+        echo "Adding headers for ${LIBRARY_NAME}"
+        INPUT_FILES+=("-headers ${LEADER_PATH}../../../include")
+    fi
+
+    for ARCH_PATH in "${PATHS[@]:1}"; do
+        THIS_FILE="${ARCH_PATH}/${LIBRARY_NAME}.a"
+        if [[ -f "${THIS_FILE}" ]]; then
+            INPUT_FILES+=(" -library ${THIS_FILE}")
+        else
+            echo "Error: ${THIS_FILE} does not exist."
+            exit 1
+        fi
+    done
+
+    echo "${INPUT_FILES[@]}"
+
+    eval "xcodebuild -create-xcframework ${INPUT_FILES[@]} -output ${OUTPUT_DIR}/${LIBRARY_NAME}.xcframework"
+done
