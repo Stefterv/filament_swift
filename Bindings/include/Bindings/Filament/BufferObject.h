9feeb099c72eeb237e892717ff58a80088067ed7
Stef Tervelde <stef@steftervelde.nl>
2023-09-10 17:27:41 +0200

    Buffer Object Headers

--- Bindings/include/Bindings/Filament/BufferObject.h
+++ Bindings/include/Bindings/Filament/BufferObject.h
@@ -5,6 +5,7 @@
 //  Created by Stef Tervelde on 10.09.23.
 //
 #import <Foundation/Foundation.h>
+#import "Engine.h"
 
 #ifndef BufferObject_h
 #define BufferObject_h
@@ -14,6 +15,21 @@
 @property (nonatomic, readonly, nonnull) void* BufferObject  NS_SWIFT_UNAVAILABLE("Don't access the raw pointers");
 - (nonnull id) init: (nonnull void*) BufferObject NS_SWIFT_UNAVAILABLE("Instances are created internally");
 - (nonnull id) init NS_UNAVAILABLE;
+
+/**
+ * Asynchronously copy-initializes a region of this BufferObject from the data provided.
+ *
+ * @param engine Reference to the filament::Engine associated with this BufferObject.
+ * @param buffer A BufferDescriptor representing the data used to initialize the BufferObject.
+ * @param byteOffset Offset in bytes into the BufferObject
+ */
+- (void) setBuffer: (nonnull Engine*) engine :(nonnull NSData*) buffer :(uint32_t) byteOffset;
+
+/**
+ * Returns the size of this BufferObject in elements.
+ * @return The maximum capacity of the BufferObject.
+ */
+- (size_t) getByteCount;
 
 @end
 
--- Bindings/include/Bindings/Filament/BufferObjectBuilder.h
+++ Bindings/include/Bindings/Filament/BufferObjectBuilder.h
@@ -5,9 +5,15 @@
 //  Created by Stef Tervelde on 10.09.23.
 //
 #import <Foundation/Foundation.h>
+#import "Engine.h"
+#import "BufferObject.h"
 
 #ifndef BufferObjectBuilder_h
 #define BufferObjectBuilder_h
+
+typedef NS_ENUM(NSInteger, BindingType) {
+    Vertex = 0
+};
 
 @interface BufferObjectBuilder : NSObject
 
@@ -15,7 +21,36 @@
 - (nonnull id) init: (nonnull void*) Builder NS_SWIFT_UNAVAILABLE("Instances are created internally");
 - (nonnull id) init NS_UNAVAILABLE;
 
-#warning("TODO")
+/**
+ * Size of the buffer in bytes.
+ * @param byteCount Maximum number of bytes the BufferObject can hold.
+ * @return A reference to this Builder for chaining calls.
+ */
+- (nonnull instancetype) size: (uint32_t) byteCount;
+
+/**
+ * The binding type for this buffer object. (defaults to VERTEX)
+ * @param BindingType Distinguishes between SSBO, VBO, etc. For now this must be VERTEX.
+ * @return A reference to this Builder for chaining calls.
+ */
+- (nonnull instancetype) bindingType: (BindingType) bindingType;
+
+/**
+ * Creates the BufferObject and returns a pointer to it. After creation, the buffer
+ * object is uninitialized. Use BufferObject::setBuffer() to initialize it.
+ *
+ * @param engine Reference to the filament::Engine to associate this BufferObject with.
+ *
+ * @return pointer to the newly created object or nullptr if exceptions are disabled and
+ *         an error occurred.
+ *
+ * @exception utils::PostConditionPanic if a runtime error occurred, such as running out of
+ *            memory or other resources.
+ * @exception utils::PreConditionPanic if a parameter to a builder function was invalid.
+ *
+ * @see IndexBuffer::setBuffer
+ */
+- (nullable BufferObject*) build :(nonnull Engine*) engine;
 
 
 @end
